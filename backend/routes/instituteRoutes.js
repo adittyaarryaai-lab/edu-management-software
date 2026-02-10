a674f35031c3d13b4d62ab6b776edac44f866cd0
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-02-10 10:53:17 +0530

    Day 6: setup multi-tenant architecture, JWT auth, and RBAC middleware

--- backend/routes/instituteRoutes.js
+++ backend/routes/instituteRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 // We import the logic from the controller we created in Step 1
 const { registerInstitute } = require('../controllers/instituteController');
-
+const { protect, authorize } = require('../middleware/authMiddleware');
 /**
  * @route   POST /api/institutes/register
  * @desc    Register a new Institute and create its first Admin
  * @access  Public (In a real SaaS, this might be restricted to Super Admin later)
  */
-router.post('/register', registerInstitute);
 
+router.post('/register', protect, authorize('super-admin'), registerInstitute);
 module.exports = router;
